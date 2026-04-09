f6d066798012021bbda0096acbce11b8d1392717
yudaipe <yudaipe01@gmail.com>
2026-04-09 20:10:23 +0900

    chore: bump version to v2.4.1 (versionCode=37)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shogun.speedtest"
         minSdk = 26
         targetSdk = 34
-        versionCode = 36
-        versionName = "2.4.0"
+        versionCode = 37
+        versionName = "2.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
